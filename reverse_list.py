6e4ac4a209b5075b163e1c6f622629114c8bd498
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-02-18 21:54:45 -0800

    second

--- reverse_list.py
+++ reverse_list.py
@@ -1,3 +1,7 @@
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 2/18/2022
+# Description: A function named reverse_list that takes as a parameter a list and reverses the order of the elements in that list.
 def reverse_list(l):
     i = 0
     j = len(l) - 1
